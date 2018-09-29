b8d1144749ba463bb3a41e92d3301de422ee0d66
Brent Langston <brentley@oufan.com>
2018-09-29 10:48:16 -0500

    remove terraform

--- terraform/acm_certificate.tf
+++
@@ -1,7 +0,0 @@
-module "acm_request_certificate" {
-  source  = "cloudposse/acm-request-certificate/aws"
-  version = "0.1.3"
-  domain_name                      = "${var.dnsdomain}"
-  proces_domain_validation_options = "false" # if r53 domain is enabled, then process dns validation, otherwise don't
-  ttl                              = "300"
-}
--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-output "cloudfront URL" {
-  value = "${aws_cloudfront_distribution.distribution.domain_name}"
-}
-output "Build Badge URL" {
-  value = "${aws_codebuild_project.codebuild.badge_url}"
-}
