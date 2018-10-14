8e12fa057edede0d77f7e07e74da0476fc1e9a88
Brent Langston <blangs@amazon.com>
2018-10-14 11:07:48 -0500

    Add cloudformation sample and shortcodes (#43)
    * updating with an example of referencing cf templates
    
    * clean up old button references
    
    * adding sample templates
    
    * removing example cf template file
    
    * Revert "removing example cf template file"
    
    This reverts commit 881e78ef59adc541dd31ed8167db1d4e6ee8f03c.
    
    * better wording from @dalbhanj

--- codebuild-deploy.sh
+++ codebuild-deploy.sh
@@ -2,6 +2,7 @@
 
 set -vex
 [ -n "${PRIMARY_BUCKET}" ] || exit 1
+[ -n "${TEMPLATE_BUCKET}" ] || exit 1
 [ -n "${CODEBUILD_GIT_CLEAN_BRANCH}" ] || exit 1
 [ -n "${CLOUDFRONT}" ] || exit 1
 
@@ -12,10 +13,12 @@ PRIMARY_BUCKET=$(echo $PRIMARY_BUCKET | cut -f1,2 -d.)
 if [ "$CODEBUILD_GIT_CLEAN_BRANCH" = "master" ] ; then
   aws s3 sync public/ s3://${PRIMARY_BUCKET}/ --cache-control 'max-age=3600, public' --exclude '*' --include '*.html' --include '*.xml';
   aws s3 sync public/ s3://${PRIMARY_BUCKET}/ --cache-control 'max-age=86400, public' --exclude '*.html' --exclude '*.xml';
+  aws s3 sync templates/ s3://${TEMPLATE_BUCKET}/${CODEBUILD_GIT_CLEAN_BRANCH}/ --delete --cache-control 'max-age=86400, public' --acl public-read;
   aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT} --paths /\*
 else
   export DEPLOY_DIR=${CODEBUILD_GIT_CLEAN_BRANCH};
   aws s3 sync public/ s3://${PRIMARY_BUCKET}/${DEPLOY_DIR}/ --delete --cache-control 'max-age=3600, public' --exclude '*' --include '*.html' --include '*.xml';
   aws s3 sync public/ s3://${PRIMARY_BUCKET}/${DEPLOY_DIR}/ --delete --cache-control 'max-age=86400, public' --exclude '*.html' --exclude '*.xml';
+  aws s3 sync templates/ s3://${TEMPLATE_BUCKET}/${CODEBUILD_GIT_CLEAN_BRANCH}/ --delete --cache-control 'max-age=86400, public' --acl public-read;
   aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT} --paths /${DEPLOY_DIR}\*
 fi
